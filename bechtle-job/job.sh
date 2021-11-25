696bc3071eb54a63bfaa145951c2e664819a5462
Tobias Koch <pipuhop@gmail.com>
2021-11-25 19:06:03 +0100

    v1.0

--- bechtle-job/job.sh
+++ bechtle-job/job.sh
@@ -2,7 +2,10 @@ while true
 do
   IP=$(curl --silent ifconfig.io)
   DATE=$(date +%F_%T)
-  echo $IP $DATE > /data/index.html
+  cp /opt/index.html.org /data/index.html
+  cp -u /opt/logo-bechtle.png /data
+  sed -i "/NGINX is working/a <h2>IP: ${IP}<\h2>" /data/index.html
+  sed -i "/NGINX is working/a <h2>DATE: ${DATE}<\h2>" /data/index.html
   sleep 2
 done
   
